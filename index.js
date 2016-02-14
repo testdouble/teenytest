691865fab5944547e07ec3c9626de8f58095687a
Justin Searls <searls@gmail.com>
2016-02-13 23:10:31 -0500

    Dramatically refactor async design.
    A few fun notes:
    
    * lib/runner - An abstraction wrapping our use of async.series, which:
      * takes a heterogeneous array of (1) user-defined test hooks/functions 
    and (2) arrays of such functions. These can be arbitrarily deeply nested
    which I did both to separate the arrangement of the actions from their
    execution and to pave the way for a runner that supports arbitrarily
    nested tests (as opposed to just single-layer xUnit style tests). 
      * build a results object which has both a global flag to indicate 
    whether ANYTHING failed (in which case the whole test is a failure) as
    well as a "nestedPassing" collection of switches localized to the 
    nesting. For instance, if a global beforeAll fails, ALL the tests should
    be considered failures. If a test module's beforeAll fails, all the
    tests _defined inside that module_ should fail. And so on. This was a 
    super tricky concept to encode, and I ultimately settled for making it
    mutable (which means test parallelizing or reordering won't be easily 
    possible)
    
    * lib/build-test-actions - build the nested arrays of test functions
    that are fed to the runner
    
    * lib/build-test-helper - builds the test helper; if one is provided
    by the user, its file will be available for any error messages of global
    failures so users can more easily track the error down
    
    * lib/user-function-async-wrapper-factory - given the options and log,
    returns a function which can be used to wrap any user function (e.g.
    a hook or a test function) and make it quack as if it's asynchronous.
    For genuinely async functions, it will enforce the test's timeout for 
    them as well. Additionally, it'll log any failures against the mutable
    `results` object instantiated by the runner, separating "error" messages
    from "failure" messages (e.g. if the test fails, print the `not ok` even
    if the test itself has no errror)

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var _ = require('lodash')
-var path = require('path')
-var async = require('async')
-var userActionsFactory = require('./lib/user-actions-factory')
 var buildTestModules = require('./lib/build-test-modules')
+var buildTestActions = require('./lib/build-test-actions')
+var buildTestHelper = require('./lib/build-test-helper')
+var runner = require('./lib/runner')
+var userFunctionAsyncWrapperFactory = require('./lib/user-function-async-wrapper-factory.js')
 
 module.exports = function (testGlob, userOptions, cb) {
   if (arguments.length < 3) { cb = userOptions; userOptions = {} }
@@ -15,121 +16,20 @@ module.exports = function (testGlob, userOptions, cb) {
   }, userOptions, helper.options)
   var log = options.output
   var testModules = buildTestModules(testGlob, cwd)
-  var userActions = userActionsFactory(options)
-  var passed = true
 
   log('TAP version 13')
   log('1..' + _(testModules).map('tests.length').sum())
 
-  var thisTestSuitePassed = true
-  var testSuiteErrorLogger = function (e, action) {
+  runner(buildTestActions(testModules, helper), userFunctionAsyncWrapperFactory(options, log), function (e, result) {
     if (e) {
-      thisTestSuitePassed = passed = false
-      log(' An error occurred in ' + action.description)
-      log('  ---')
-      log('  message: ' + e.message)
-      log('  stacktrace:', e.stack)
-      log('  ...')
-    }
-  }
-  var testSuiteActions = [
-    userActions.wrap(helper.beforeAll, {description: 'global test helper beforeAll hook'}, testSuiteErrorLogger),
-    function (cb) {
-      var testModuleActions = _(testModules).map(function (testModule, testModuleIndex) {
-        var thisTestModulePassed = true
-        var testModuleErrorLogger = function (e, action) {
-          if (e) {
-            thisTestModulePassed = passed = false
-            log(' An error occurred in ' + action.description)
-            log('  ---')
-            log('  message: ' + e.message)
-            log('  stacktrace:', e.stack)
-            log('  ...')
-          }
-        }
-        return [
-          userActions.wrap(testModule.beforeAll, addHookDescription('beforeAll', testModule), testModuleErrorLogger),
-          function (cb) {
-            var testFunctionActions = _(testModule.tests).map(function (test, i) {
-              var thisTestPassed = true
-              var testFunctionErrorLogger = function (e, action) {
-                if (e) {
-                  thisTestPassed = passed = false
-                  log(' An error occurred in ' + action.description)
-                  log('  ---')
-                  log('  message: ' + e.message)
-                  log('  stacktrace:', e.stack)
-                  log('  ...')
-                }
-              }
-
-              return [
-                userActions.wrap(helper.beforeEach, addHookDescription('global beforeEach', test, i), testFunctionErrorLogger),
-                userActions.wrap(testModule.beforeEach, addHookDescription('beforeEach', test, i), testFunctionErrorLogger),
-                function (cb) {
-                  userActions.invoke(test.testFunction, test, function (e) {
-                    if (!e && thisTestPassed && thisTestModulePassed && thisTestSuitePassed) {
-                      log('ok ' + test.description)
-                    } else {
-                      passed = false
-                      log('not ok ' + test.description)
-                      if (e) {
-                        log('  ---')
-                        log('  message: ' + e.message)
-                        log('  stacktrace:', e.stack)
-                        log('  ...')
-                      }
-                    }
-                    cb(null)
-                  })
-                },
-                userActions.wrap(helper.afterEach, addHookDescription('global afterEach', test, i), testFunctionErrorLogger),
-                userActions.wrap(testModule.afterEach, addHookDescription('afterEach', test, i), testFunctionErrorLogger)
-              ]
-            }).flatten().value()
-
-            async.series(testFunctionActions, cb)
-          },
-          userActions.wrap(testModule.afterAll, addHookDescription('afterAll', testModule), testModuleErrorLogger)
-        ]
-      }).flatten().value()
-
-      async.series(testModuleActions, cb)
-    },
-    userActions.wrap(helper.afterAll, {description: 'global test helper afterAll hook'}, testSuiteErrorLogger)
-  ]
-
-  async.series(testSuiteActions, function (e) {
-    if (e) {
-      passed = false
       log('A fatal error occurred!')
       log('  ---')
-      log('  message: ' + e.message)
+      log('  message: ' + e.message || e.toString())
       log('  stacktrace:', e.stack)
       log('  ...')
     }
 
-    cb(e, passed)
+    cb(e, result)
   })
-}
-
-var buildTestHelper = function (helperPath, cwd) {
-  return _.assign({}, {
-    beforeAll: function () {},
-    afterAll: function () {},
-    beforeEach: function () {},
-    afterEach: function () {},
-    options: {}
-  }, helperPath ? require(path.resolve(cwd, helperPath)) : {})
-}
-
-var addHookDescription = function (hookType, testOrModule, index) {
-  return addDescription(testOrModule, hookDescription(hookType, testOrModule, index))
-}
-var addDescription = function (obj, description) {
-  return _.assign({}, obj, {description: description})
-}
-var hookDescription = function (hookType, testOrModule, index) {
-  return hookType + ' hook' + (_.isNumber(index) ? ' in test #' + (index + 1) : '') + ' in `' + testOrModule.file + '`'
 }
 
