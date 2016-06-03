2bf3cad15035f5421ed130bbd6c0dc3655016da2
Justin Searls <searls@gmail.com>
2016-06-04 00:09:05 +0200

    WIP it good

--- example/test/lib/multi-word-names.js
+++ example/test/lib/multi-word-names.js
@@ -1,6 +1,13 @@
+var assert = require('assert')
+
 module.exports = {
   'a long passing test name': function () {
+    var a = 1
+    var b = 2
 
+    var result = a + b
+
+    assert.equal(result, 3)
   },
   'a long failing test name': function () {
     throw new Error('I fail')
