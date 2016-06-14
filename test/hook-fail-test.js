9cdb3f6ac21d55241863bf19ca0c9a35c3dccc76
Justin Searls <searls@gmail.com>
2016-06-14 13:11:39 +0200

    Something I did in this branch fixed #16
    So, uhh, fixes #16

---
+++ test/hook-fail-test.js
@@ -0,0 +1,20 @@
+var helper = require('./support/helper')
+var assert = require('assert')
+
+module.exports = function (cb) {
+  helper.run('test/fixtures/hook-fail.js', function (er, result, log) {
+    assert.equal(result, false)
+    log.assert(
+      'TAP version 13',
+      '1..1',
+      ' An error occurred in test hook: module beforeEach defined in `test/fixtures/hook-fail.js`',
+      '  ---',
+      '  message: Bad hook do not run!',
+      /stacktrace: Error: Bad hook do not run/,
+      '  ...',
+      'not ok 1 - "thisIsNotOk" - test #1 in `test/fixtures/hook-fail.js`'
+    )
+    cb(er)
+  })
+}
+
