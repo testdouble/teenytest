testdouble/teenytest

commit 73b42361c129be85e3e48457d07b03dd86944654

Dramatically increase the complexity of everything
This was certainly one way to make sure the right hooks are firing when
we want. 

Particularly nasty is the use of `distanceFromTest` as a crude 
instrument to determine whether an afterEach is at a deeper depth than
an earlier error (to avoid running them unnecessarily). Just ugly as 
hell.

lib/build-test-actions.js
@@ -54,15 +54,13 @@ var actionsForTestFunctions = function (test, ancestors, helper, stats) {
     suiteNames: test.ancestorNames,
     children: _.flatten([
       actionForHook(helper.beforeEach, 'global', 'beforeEach', test.context, helper.file),
-      _.map(ancestors, function (ancestor) {
-        return actionForHook(ancestor.beforeEach, (ancestor.name || 'module'),
-                             'beforeEach', test.context, ancestor.file)
+      _.map(ancestors, function (ancestor, i) {
+        return actionForAncestorHook('beforeEach', ancestor, i, test)
       }),
       actionForTest(test, stats),
       actionForHook(helper.afterEach, 'global', 'afterEach', test.context, helper.file),
-      _(ancestors).reverse().map(function (ancestor) {
-        return actionForHook(ancestor.afterEach, (ancestor.name || 'module'),
-                             'afterEach', test.context, ancestor.file)
+      _(ancestors).reverse().map(function (ancestor, i) {
+        return actionForAncestorHook('afterEach', ancestor, i, test)
       }).value()
     ]),
     callable: function (cb) {
@@ -73,7 +71,12 @@ var actionsForTestFunctions = function (test, ancestors, helper, stats) {
   return testActions
 }
 
-var actionForHook = function (helperFunc, name, hookType, context, file) {
+function actionForAncestorHook (type, ancestor, distance, test) {
+  return actionForHook(ancestor[type], (ancestor.name || 'module'),
+                       type, test.context, ancestor.file, distance)
+}
+
+var actionForHook = function (helperFunc, name, hookType, context, file, distance) {
   var action = {
     name: name,
     type: 'userFunction',
@@ -82,7 +85,8 @@ var actionForHook = function (helperFunc, name, hookType, context, file) {
     description: 'test hook: ' + name + ' ' + hookType +
                  (file ? ' defined in `' + file + '`' : ''),
     callable: helperFunc.bind(context || {}),
-    isAssociatedWithATest: !_.isNil(context)
+    isAssociatedWithATest: !_.isNil(context),
+    distanceFromTest: distance || 0
   }
 
   return action
@@ -95,7 +99,8 @@ var actionForTest = function (test, stats) {
     subType: 'test',
     description: test.description(stats.suiteCount, stats.fileCounts[test.file]),
     callable: test.testFunction.bind(test.context),
-    isAssociatedWithATest: true
+    isAssociatedWithATest: true,
+    distanceFromTest: 0
   }
 
   return action

lib/plugins/internal/results.js
@@ -19,11 +19,13 @@ module.exports = function (log) {
       wrappers: {
         userFunction: function (runUserFunction, metadata, cb) {
           if (!shouldBeRun(results, metadata)) return cb(null)
+
           runUserFunction(function (er) {
             if (er) {
               markFailure(results, metadata, er)
               if (!metadata.isAssociatedWithATest) {
-                logError(log, metadata, _.last(results.nestedResults).errors.pop())
+                logError(log, metadata,
+                         _.last(_.last(results.nestedResults).errors))
               }
             }
             cb(null)
@@ -44,7 +46,9 @@ module.exports = function (log) {
             log((failed ? 'not ' : '') + 'ok ' + metadata.description)
 
             _.each(_.last(results.nestedResults).errors, function (er, i) {
-              logError(log, metadata, er)
+              if (er.metadata.isAssociatedWithATest) {
+                logError(log, metadata, er)
+              }
             })
 
             results.nestedResults.pop()
@@ -100,7 +104,7 @@ function logError (log, metadata, errorSummary) {
 
 function shouldBeRun (results, metadata) {
   if (!failedYet(results) ||
-      (testFuncErrored(results) && metadata.hookType === 'afterEach') ||
+      shouldRunAfterEach(results, metadata) ||
       metadata.hookType === 'afterAll') {
     return true
   } else {
@@ -108,8 +112,13 @@ function shouldBeRun (results, metadata) {
   }
 }
 
-function testFuncErrored (results) {
+function shouldRunAfterEach (results, metadata) {
+  if (metadata.hookType !== 'afterEach') return false
+
   var errors = _.last(results.nestedResults).errors
-  return _.some(errors, 'metadata.isAssociatedWithATest')
+  return _(errors).map('metadata').some(function (errorMetadata) {
+    return errorMetadata.isAssociatedWithATest &&
+           errorMetadata.distanceFromTest <= metadata.distanceFromTest
+  })
 }
 

test/fixtures/reliable-hooks.js
@@ -23,6 +23,28 @@ module.exports = {
     test: mark('beforeAllFails.test'),
     afterEach: mark('beforeAllFails.afterEach'),
     afterAll: mark('beforeAllFails.afterAll')
+  },
+  beforeEachFails: {
+    nest: {
+      beforeAll: mark('beforeEachFails.nest.beforeAll'),
+      beforeEach: mark('beforeEachFails.nest.beforeEach'),
+      test: mark('beforeEachFails.nest.test'),
+      afterEach: mark('beforeEachFails.nest.afterEach'),
+      afterAll: mark('beforeEachFails.nest.afterAll')
+    },
+    beforeAll: mark('beforeEachFails.beforeAll'),
+    beforeEach: fail('beforeEachFails.beforeEach'),
+    test: mark('beforeEachFails.test'),
+    test2: mark('beforeEachFails.test2'),
+    afterEach: mark('beforeEachFails.afterEach'),
+    afterAll: mark('beforeEachFails.afterAll')
+  },
+  testFails: {
+    beforeAll: mark('testFails.beforeAll'),
+    beforeEach: mark('testFails.beforeEach'),
+    test: fail('testFails.test'),
+    afterEach: mark('testFails.afterEach'),
+    afterAll: mark('testFails.afterAll')
   }
 }
 

test/reliable-hooks-test.js
@@ -2,6 +2,7 @@ var helper = require('./support/helper')
 
 module.exports = function (cb) {
   helper.run('test/fixtures/reliable-hooks.js', function (er, result, log) {
+    // Without failure, it all runs (ofc)
     helper.deepEqual(global.__results.happy, {
       beforeAll: true,
       beforeEach: true,
@@ -10,6 +11,7 @@ module.exports = function (cb) {
       afterAll: true
     })
 
+    // If a beforeAll fails, only sibling afterAll runs, everything else stops
     helper.deepEqual(global.__results.beforeAllFails, {
       beforeAll: 'fail',
       beforeEach: false,
@@ -23,6 +25,32 @@ module.exports = function (cb) {
         afterEach: false,
         afterAll: false
       }
+    })
+
+    // If a beforeEach fails only sibling afterEach runs (the *Alls already ran)
+    helper.deepEqual(global.__results.beforeEachFails, {
+      beforeAll: true,
+      beforeEach: 'fail',
+      test: false,
+      test2: false,
+      afterEach: true,
+      afterAll: true,
+      nest: {
+        beforeAll: true,
+        beforeEach: false,
+        test: false,
+        afterEach: false,
+        afterAll: true
+      }
+    })
+
+    // If a test fails, everything else runs
+    helper.deepEqual(global.__results.testFails, {
+      beforeAll: true,
+      beforeEach: true,
+      test: 'fail',
+      afterEach: true,
+      afterAll: true
     })
 
     delete global.__results

commit ce79768e87a097eadb199d7e7658689a2deca157

Fix the nesting levels—these were very very wrong
Not sure how this happened, but the nesting levels introduced in 
73b42361 were counting beforeEach's completely backwards

lib/build-test-actions.js
@@ -56,15 +56,9 @@ var actionsForTestFunctions = function (test, ancestors, helper, stats) {
     suiteNames: test.ancestorNames,
     isAssociatedWithATest: true,
     children: _.flatten([
-      actionForHook(helper.beforeEach, 'global', 'beforeEach', test.context, helper.file),
-      _.map(ancestors, function (ancestor, i) {
-        return actionForAncestorHook('beforeEach', ancestor, i, test)
-      }),
+      beforeEachActions(helper, test, ancestors),
       actionForTest(test, stats),
-      actionForHook(helper.afterEach, 'global', 'afterEach', test.context, helper.file),
-      _(ancestors).reverse().map(function (ancestor, i) {
-        return actionForAncestorHook('afterEach', ancestor, i, test)
-      }).value()
+      afterEachActions(helper, test, ancestors)
     ]),
     callable: function (cb) {
       async.series(_.map(testActions.children, 'callable'), cb)
@@ -72,6 +66,24 @@ var actionsForTestFunctions = function (test, ancestors, helper, stats) {
   }
 
   return testActions
+}
+
+function beforeEachActions (helper, test, ancestors) {
+  var maxDistance = ancestors.length
+  var globalBeforeEach = actionForHook(helper.beforeEach, 'global', 'beforeEach',
+                                       test.context, helper.file, maxDistance)
+  return [globalBeforeEach].concat(_.map(ancestors, function (ancestor, i) {
+    return actionForAncestorHook('beforeEach', ancestor, maxDistance - 1 - i, test)
+  }))
+}
+
+function afterEachActions (helper, test, ancestors) {
+  var maxDistance = ancestors.length
+  var globalAfterEach = actionForHook(helper.afterEach, 'global', 'afterEach',
+                                      test.context, helper.file, maxDistance)
+  return _(ancestors).reverse().map(function (ancestor, i) {
+    return actionForAncestorHook('afterEach', ancestor, i, test)
+  }).value().concat(globalAfterEach)
 }
 
 function actionForAncestorHook (type, ancestor, distanceFromTest, test) {

lib/plugins/internal/results.js
@@ -33,18 +33,18 @@ module.exports = function (log) {
           })
         },
         test: function (runTest, metadata, cb) {
-          if (failedYet(results)) {
-            log('not ok ' + metadata.description + ' [SKIPPED]')
+          if (testFailed(results)) {
+            tap13.test(metadata.description, {pass: false, skip: true})
             return cb(null)
           }
+          // resultsStore.nest()
           results.nestedResults.push({passing: true, errors: []})
           runTest(function (er) {
             if (er) {
               markFailure(results, metadata, er)
             }
 
-            var failed = failedYet(results)
-            log((failed ? 'not ' : '') + 'ok ' + metadata.description)
+            tap13.test(metadata.description, {pass: !testFailed(results)})
 
             var errors = _(results.nestedResults).map('errors').last()
             _.each(errors, function (er, i) {
@@ -53,12 +53,14 @@ module.exports = function (log) {
               }
             })
 
+            // resultsStore.unNest()
             results.nestedResults.pop()
             cb(null)
           })
         },
         suite: function (runSuite, metadata, cb) {
-          if (failedYet(results)) return cb(null)
+          if (testFailed(results)) return cb(null)
+          // resultsStore.nest()
           results.nestedResults.push({passing: true, errors: []})
           runSuite(function (er) {
             if (er) {
@@ -66,6 +68,7 @@ module.exports = function (log) {
               tap13.error(er, 'suite: "' + metadata.name + '" in ' +
                               '`' + metadata.file + '`')
             }
+            // resultsStore.unNest()
             results.nestedResults.pop()
             cb(null)
           })
@@ -88,12 +91,12 @@ function markFailure (results, metadata, er) {
   })
 }
 
-function failedYet (results) {
+function testFailed (results) {
   return !_(results.nestedResults).map('passing').every()
 }
 
 function shouldBeRun (results, metadata) {
-  if (!failedYet(results) ||
+  if (!testFailed(results) ||
       shouldRunAfterEach(results, metadata) ||
       metadata.hookType === 'afterAll') {
     return true
@@ -104,7 +107,7 @@ function shouldBeRun (results, metadata) {
 
 function shouldRunAfterEach (results, metadata) {
   if (metadata.hookType !== 'afterEach') return false
-
+//  return resultsStore.afterEachCleanupNecessary(metadata.distanceFromTest)
   var errors = _.last(results.nestedResults).errors
   return _(errors).map('metadata').some(function (errorMetadata) {
     return errorMetadata.isAssociatedWithATest &&

lib/report/results-store.js
@@ -1,0 +1,45 @@
+var _ = require('lodash')
+
+var results
+
+module.exports = {
+  initialize: function () {
+    results = {
+      overallPassing: true,
+      nestedResults: [{passing: true, errors: []}]
+    }
+  },
+  nest: function () {
+    results.nestedResults.push({passing: true, errors: []})
+  },
+  unNest: function () {
+    results.nestedResults.pop()
+  },
+  testFailed: function () {
+    return !_(results.nestedResults).map('passing').every()
+  },
+
+  // This is a confusing function:
+  //
+  // an afterEach should be run if a beforeEach or test function at the same or
+  // deeper nesting level failed. "Distance" in this case is the number of layers
+  // of nesting away from the test function itself. For instance:
+  // foo: {
+  //   bar: {
+  //     baz: function () {}, // test, distance 0
+  //     afterEach: function () {} // distance 0
+  //   },
+  //   afterEach: function () {} // distance 1
+  // }
+  //
+  // In the above example, if test 'foo bar baz' fails, it'll be at a distance
+  // less than the final afterEach, so that afterEach ought to be run.
+  afterEachCleanupNecessary: function (afterEachDistanceFromTest) {
+    var errors = _.last(results.nestedResults).errors
+    return _(errors).map('metadata').some(function (errorMetadata) {
+      return errorMetadata.isAssociatedWithATest &&
+             errorMetadata.distanceFromTest <= afterEachDistanceFromTest
+    })
+  }
+
+}

lib/report/tap13.js
@@ -1,7 +1,14 @@
 var _ = require('lodash')
 
-module.exports = Tap13 = function Tap13 (log) {
+function Tap13 (log) {
   this.log = log
+}
+module.exports = Tap13
+
+Tap13.prototype.test = function (description, info) {
+  this.log((info.pass ? '' : 'not ') + 'ok ' +
+           description +
+           (info.skip ? ' [SKIPPED]' : ''))
 }
 
 Tap13.prototype.error = function (er, standaloneDescription) {
@@ -12,5 +19,4 @@ Tap13.prototype.error = function (er, standaloneDescription) {
   this.log('  message: ' + er.message || er.toString())
   this.log('  stacktrace:', er.stack)
   this.log('  ...')
-
 }

test/fixtures/multi-error.js
@@ -1,0 +1,17 @@
+module.exports = {
+  beforePlusAfter: {
+    beforeEach: function () { throw new Error('A') },
+    test: function () { throw new Error('B') },
+    afterEach: function () { throw new Error('C') }
+  },
+  afterRuinsIt: {
+    foo: {
+      test: function () {}
+    },
+    afterEach: function () { throw new Error('D') }
+  },
+  testPlusAfter: {
+    test: function () { throw new Error('E') },
+    afterEach: function () { throw new Error('F') }
+  }
+}

test/multi-error-test.js
@@ -1,0 +1,37 @@
+var helper = require('./support/helper')
+var assert = require('assert')
+
+module.exports = function (cb) {
+  helper.run('test/fixtures/multi-error.js', function (er, result, log) {
+    assert.equal(result, false)
+    log.assert(
+      'TAP version 13',
+      '1..3',
+      'not ok 1 - "beforePlusAfter test" - test #1 in `test/fixtures/multi-error.js`',
+      '  ---',
+      '  message: A',
+      /stacktrace: Error: A/,
+      '  ...',
+      '  ---',
+      '  message: C',
+      /stacktrace: Error: C/,
+      '  ...',
+      'not ok 2 - "afterRuinsIt foo test" - test #2 in `test/fixtures/multi-error.js`',
+      '  ---',
+      '  message: D',
+      /stacktrace: Error: D/,
+      '  ...',
+      'not ok 3 - "testPlusAfter test" - test #3 in `test/fixtures/multi-error.js`',
+      '  ---',
+      '  message: E',
+      /stacktrace: Error: E/,
+      '  ...',
+      '  ---',
+      '  message: F',
+      /stacktrace: Error: F/,
+      '  ...'
+    )
+    cb(er)
+  })
+}
+

test/plugin-test.js
@@ -25,7 +25,6 @@ module.exports = function (finalCallbackPhew) {
             // TODO one solution may be to provide a second arg to plugins runTest
             // TODO cb that includes the results (nested passing + errors?)
             // TODO how would we do that?
-            console.log("HEYO", metadata, er)
             cb(new Error(
               'Test "' + metadata.name + '" has "pending" in its name but it did' +
               ' not fail! Perhaps you have yet to implement a failing test or' +

test/support/runner.js
@@ -16,5 +16,5 @@ async.series(_.map(glob.sync(globLocator), function (file) {
   }
 }), function (er) {
   if (er) { throw er }
-  console.log('looks good!')
+  console.log('Looks good!')
 })