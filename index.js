860be63758c704502e716c0387e9856b5ead39be
Cliff Pruitt <cliff.pruitt@cliffpruitt.com>
2019-09-24 11:20:37 -0400

    Remove debugger statement

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var run = require('./lib/run')
 module.exports = function (testLocator, userOptions, cb) {
   if (arguments.length < 3) { cb = userOptions; userOptions = {} }
   var config = configure(testLocator, userOptions)
-  debugger
   run(plan(prepare(config)), config, cb)
 }
 
