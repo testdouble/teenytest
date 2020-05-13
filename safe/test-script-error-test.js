49b8a8bd63417373c32dd54b781a70bb8c36b159
Gil Tayar <gil@tayar.org>
2020-05-13 21:15:49 +0300

    fix assert to work in Node >= 8

--- safe/test-script-error-test.js
+++ safe/test-script-error-test.js
@@ -4,8 +4,8 @@ var assert = require('assert')
 module.exports = function (cb) {
   helper.run('safe/fixtures/syntax-error-test.js', function (er, result, log) {
     assert.strictEqual(result, undefined)
-    assert.match(log.read()[0], /Error: this is a loading error/)
-    assert.match(log.read()[0], /syntax-error-test\.js/)
+    assert(log.read()[0].match(/Error: this is a loading error/))
+    assert(log.read()[0].match(/syntax-error-test\.js/))
     cb()
   })
 }
